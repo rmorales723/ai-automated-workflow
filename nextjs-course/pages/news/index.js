7cdd586ded242b893722ea726644933c74e931dd
robert morales <rmoralfit@gmail.com>
2022-03-28 20:45:53 -0400

    created index.js news.js and nested something-important.js files

---
+++ nextjs-course/pages/news/index.js
@@ -0,0 +1,9 @@
+// import { getDomainLocale } from "next/dist/shared/lib/router/router";
+
+// our-domain.com/news
+
+function NewsPage() {
+    return <h1>The News Page</h1>
+}
+
+export default NewsPage;
